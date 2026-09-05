40142a2e58c98deea844fa708db44fd89306673a
skrishna-code <krishnasutar026@gmail.com>
2026-09-05 08:38:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,98 +1,98 @@
-const msg = new SpeechSynthesisUtterance();
-let voices = [];
+// const msg = new SpeechSynthesisUtterance();
+// let voices = [];
 
-const voicesDropdown = document.querySelector('[name="voice"]');
-const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector('#speak');
-const stopButton = document.querySelector('#stop');
-
-
-// Get all available voices
-function populateVoices() {
-  voices = speechSynthesis.getVoices();
-
-  voicesDropdown.innerHTML = "";
-
-  if (voices.length === 0) {
-    const option = document.createElement("option");
-    option.textContent = "No voices available";
-    option.value = "";
-    voicesDropdown.appendChild(option);
-    return;
-  }
-
-  voices.forEach((voice, index) => {
-    const option = document.createElement("option");
-
-    option.value = index;
-    option.textContent = `${voice.name} (${voice.lang})`;
-
-    voicesDropdown.appendChild(option);
-  });
-
-  // Select first voice by default
-  msg.voice = voices[0];
-}
+// const voicesDropdown = document.querySelector('[name="voice"]');
+// const options = document.querySelectorAll('[type="range"], [name="text"]');
+// const speakButton = document.querySelector('#speak');
+// const stopButton = document.querySelector('#stop');
 
 
-// Populate voices when available
-populateVoices();
+// // Get all available voices
+// function populateVoices() {
+//   voices = speechSynthesis.getVoices();
 
-speechSynthesis.addEventListener("voiceschanged", populateVoices);
+//   voicesDropdown.innerHTML = "";
 
+//   if (voices.length === 0) {
+//     const option = document.createElement("option");
+//     option.textContent = "No voices available";
+//     option.value = "";
+//     voicesDropdown.appendChild(option);
+//     return;
+//   }
 
-// Set voice
-function setVoice() {
-  const selectedVoice = voices[voicesDropdown.value];
+//   voices.forEach((voice, index) => {
+//     const option = document.createElement("option");
 
-  if (selectedVoice) {
-    msg.voice = selectedVoice;
-  }
+//     option.value = index;
+//     option.textContent = `${voice.name} (${voice.lang})`;
 
-  // If speech is currently running, restart with new voice
-  if (speechSynthesis.speaking) {
-    speechSynthesis.cancel();
-    speak();
-  }
-}
+//     voicesDropdown.appendChild(option);
+//   });
 
-voicesDropdown.addEventListener("change", setVoice);
-
-
-// Set rate and pitch
-function setOption() {
-  msg[this.name] = this.value;
-}
-
-options.forEach(option => {
-  option.addEventListener("change", setOption);
-  option.addEventListener("input", setOption);
-});
+//   // Select first voice by default
+//   msg.voice = voices[0];
+// }
 
 
-// Speak function
-function speak() {
-  const text = document.querySelector('[name="text"]').value.trim();
+// // Populate voices when available
+// populateVoices();
 
-  // Don't speak if text is empty
-  if (!text) {
-    return;
-  }
-
-  // Stop any existing speech
-  speechSynthesis.cancel();
-
-  msg.text = text;
-
-  speechSynthesis.speak(msg);
-}
+// speechSynthesis.addEventListener("voiceschanged", populateVoices);
 
 
-// Speak button
-speakButton.addEventListener("click", speak);
+// // Set voice
+// function setVoice() {
+//   const selectedVoice = voices[voicesDropdown.value];
+
+//   if (selectedVoice) {
+//     msg.voice = selectedVoice;
+//   }
+
+//   // If speech is currently running, restart with new voice
+//   if (speechSynthesis.speaking) {
+//     speechSynthesis.cancel();
+//     speak();
+//   }
+// }
+
+// voicesDropdown.addEventListener("change", setVoice);
 
 
-// Stop button
-stopButton.addEventListener("click", function () {
-  speechSynthesis.cancel();
-});
+// // Set rate and pitch
+// function setOption() {
+//   msg[this.name] = this.value;
+// }
+
+// options.forEach(option => {
+//   option.addEventListener("change", setOption);
+//   option.addEventListener("input", setOption);
+// });
+
+
+// // Speak function
+// function speak() {
+//   const text = document.querySelector('[name="text"]').value.trim();
+
+//   // Don't speak if text is empty
+//   if (!text) {
+//     return;
+//   }
+
+//   // Stop any existing speech
+//   speechSynthesis.cancel();
+
+//   msg.text = text;
+
+//   speechSynthesis.speak(msg);
+// }
+
+
+// // Speak button
+// speakButton.addEventListener("click", speak);
+
+
+// // Stop button
+// stopButton.addEventListener("click", function () {
+//   speechSynthesis.cancel();
+// });
